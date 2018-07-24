6a0ead047ad9ba8f43e48442dae1b6e558952d9d
CTN35 <40759126+CTN35@users.noreply.github.com>
2018-07-24 15:59:47 +0200

    Plus de routes  redirect pour les details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
+import { ConsultLogementComponent } from './consult-logement/consult-logement.component';
+import { LoginComponent } from './login/login.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: AccueilComponent}
+  { path: '', component: AccueilComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'details', component: ConsultLogementComponent }
 ];
 
 @NgModule({
