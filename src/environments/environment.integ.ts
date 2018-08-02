ff06027181e79bd9af7381c426003671538a5b7d
CTN35 <40759126+CTN35@users.noreply.github.com>
2018-08-02 14:29:22 +0200

    MAJ environnements avec containers

--- src/environments/environment.integ.ts
+++ src/environments/environment.integ.ts
@@ -2,7 +2,7 @@ export const environment = {
   envName: 'integ',
   production: false,
   bpmBaseUrl: 'http://10.115.37.20:8080/kie-server/services/rest/server/',
-  bpmContainer: 'bpmc',
+  bpmContainer: 'bpmc_1.0.0',
   bpmProcessId: 'fr.edf.bpmc.AdaptationContratProcess',
   allowMultipleProcess: false
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   envName: 'dev',
   production: false,
   bpmBaseUrl: 'http://136.243.42.135:8080/kie-server/services/rest/server/',
-  bpmContainer: 'bpmc',
+  bpmContainer: 'bpmc_1.0.0',
   bpmProcessId: 'fr.edf.bpmc.AdaptationContratProcess',
   allowMultipleProcess: false
 };
